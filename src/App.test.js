f29d7b07568f916271d46779fb89004aeed040ea
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-11 19:12:01 -0800

    added simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Booking from "./components/Booking";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Renders the BookingForm heading', () => {
+  render(<Booking />);
+  const headingElement = screen.getByText("Make Your reservation");
+  expect(headingElement).toBeInTheDocument();
+})
