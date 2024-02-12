7075146cb2fd64d21e59a9b35dd7dc252372fba4
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-11 18:56:42 -0800

    added routing to main component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import Footer from "./components/Footer";
-import Main from "./components/Main";
 import Header from "./components/Header";
+import Routes from "./routes";
+
+import { BrowserRouter as Router} from 'react-router-dom';
+
 
 function App() {
     return (
-        <>
+        <Router>
             <Header/>
-            <Main/>
+            <Routes/>
             <Footer/>
-        </>
+        </Router>
     );
 }
+
+
 
 export default App;
