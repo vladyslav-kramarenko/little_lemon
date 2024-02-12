6e4a96e933017ff7862cd2060a64b490f3413037
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-11 21:33:54 -0800

    added simple thank you page

---
+++ src/components/ThankYouPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ThankYouPage() {
+    return (
+        <div>
+            <h2>Thank you for your reservation!</h2>
+        </div>
+    );
+}
+
+export default ThankYouPage;
