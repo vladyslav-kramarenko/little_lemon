73debf8ebc8603c94ee4a310aaf021c28570e26e
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-11 18:54:35 -0800

    created booking component

---
+++ src/components/Booking.jsx
@@ -0,0 +1,20 @@
+import { useReducer } from "react";
+import BookingForm from "./BookingForm";
+
+const initializeTimes = () => ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+
+const updateTimes = (state, selectedDate) => {
+    return state;
+}
+
+function Booking() {
+    const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
+
+    return (
+        <div className={"container"}>
+            <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
+        </div>
+    );
+}
+
+export default Booking;
