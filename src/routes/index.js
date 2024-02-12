91f0621e5bf0f902bbc3bd527d8eb03a5001ec44
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-11 18:53:45 -0800

    created routes component

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Route, Routes as RouterRoutes} from 'react-router-dom';
+import Menu from '../components/Menu';
+import Contacts from '../components/Contacts';
+import About from '../components/About';
+import Main from "../components/Main";
+import Booking from "../components/Booking";
+
+const Routes = () => (
+    <RouterRoutes>
+        <Route path="/menu" element={<Menu/>}/>
+        <Route path="/contacts" element={<Contacts/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/booking" element={<Booking/>}/>
+        <Route path="*" element={<Main/>}/>
+    </RouterRoutes>
+);
+
+export default Routes;
