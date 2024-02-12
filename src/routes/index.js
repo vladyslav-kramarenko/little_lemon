e2c0fd01a62faeadb1033f57bc0680adac0438e4
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-11 21:34:44 -0800

    added thank you route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Contacts from '../components/Contacts';
 import About from '../components/About';
 import Main from "../components/Main";
 import Booking from "../components/Booking";
+import ThankYouPage from "../components/ThankYouPage";
 
 const Routes = () => (
     <RouterRoutes>
@@ -12,6 +13,7 @@ const Routes = () => (
         <Route path="/contacts" element={<Contacts/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/booking" element={<Booking/>}/>
+        <Route path="/thank-you" element={<ThankYouPage />} />
         <Route path="*" element={<Main/>}/>
     </RouterRoutes>
 );
