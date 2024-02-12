ea542d0facd848d8917c5739e67f5cd6a566b2bb
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-11 18:54:53 -0800

    added links to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,14 @@
+import {Link} from "react-router-dom";
+
 function Nav() {
     return <nav>
         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Menu</li>
-            <li>Reservations</li>
-            <li>Order Online</li>
-            <li>Login</li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/booking">Reservations</Link></li>
+            <li><Link to="/order">Order Online</Link></li>
+            <li><Link to="/login">Login</Link></li>
         </ul>
     </nav>;
 }
