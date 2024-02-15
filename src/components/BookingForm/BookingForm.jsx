3c94a2e6d075fa457535bcd1f9ad960bcd5c854a
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-15 10:34:06 -0800

    moved BookingForm css to a separate file moved Booking form components to a separate folder

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import './BookingForm.css';
 
 function BookingForm({availableTimes, setSelectedDate,submitForm}) {
     const [date, setDate] = useState('');
@@ -26,9 +27,11 @@ function BookingForm({availableTimes, setSelectedDate,submitForm}) {
     };
 
     return <form onSubmit={handleSubmit} className={'booking_form'} >
+
         <label htmlFor="res-date">Choose date</label>
         <input type="date" id="res-date" value={date}
                onChange={handleDateChange}/>
+
         <label htmlFor="res-time">Choose time</label>
         <select id="res-time" value={time}
                 onChange={e => setTime(e.target.value)}>
@@ -39,16 +42,19 @@ function BookingForm({availableTimes, setSelectedDate,submitForm}) {
                 )
             }
         </select>
+
         <label htmlFor="guests">Number of guests</label>
         <input type="number" placeholder="1" min="1" max="10" id="guests"
                value={guests}
                onChange={e => setGuests(e.target.value)}/>
+
         <label htmlFor="occasion">Occasion</label>
         <select id="occasion" value={occasion}
                 onChange={e => setOccasion(e.target.value)}>
             <option>Birthday</option>
             <option>Anniversary</option>
         </select>
+
         <input type="submit" value="Make Your reservation"/>
     </form>;
 }
