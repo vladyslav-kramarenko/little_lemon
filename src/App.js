f2e4ee8af33b0e059d647c2c784e3d710be24e19
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-14 19:59:22 -0800

    refactored main component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 import Routes from "./routes";
 
 import { BrowserRouter as Router} from 'react-router-dom';
+import Nav from "./components/Nav/Nav";
 
 
 function App() {
     return (
         <Router>
-            <Header/>
+            <Nav/>
             <Routes/>
             <Footer/>
         </Router>
