8281cdbd5183880847c331f4266168b12e98da1b
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-14 19:57:52 -0800

    moved header to separate folder, added img

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+
+import Nav from "../Nav/Nav";
+import {Link} from "react-router-dom";
+import restaurantFood from './restaurantFood.jpg';
+function Header(){
+    return <header >
+        <div className={"container"}>
+        {/*    <div>*/}
+                <div className="text-section">
+                    <h1 className={"restaurant-name"}>Little Lemon</h1>
+                    <p className={"restaurant-location"}>Chicago</p>
+                    <p className={"restaurant-description"}>
+                        We are a family owned Mediterranean restaurant,
+                        focused on traditional recipes served with a modern twist.
+                    </p>
+                    <Link to="/booking">
+                        <button>Reserve a Table</button>
+                    </Link>
+                </div>
+                <div className={"image-section"}>
+                    <img src={restaurantFood} alt={"restaurantFood"}/>
+                </div>
+            {/*</div>*/}
+
+        </div>
+    </header>
+}
+
+export default Header;
