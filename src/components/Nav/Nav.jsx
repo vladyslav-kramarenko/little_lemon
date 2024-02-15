6e67521c07ed681dfe32a22b2e4fb39dd5cf47e2
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-15 13:11:13 -0800

    added burger menu to mobile version, restyled nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,23 @@
 import {Link} from "react-router-dom";
+import React from 'react';
 import logo from './Logo.png';
 import './Nav.css';
 
 function Nav() {
+    const [menuOpen, setMenuOpen] = React.useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return <nav>
-        <div className={"container"}>
-            <ul className={"nav_links"}>
-                <li><img className={"nav_logo"} src={logo} alt="Logo"/></li>
+            <img className={"nav_logo"} src={logo} alt="Little Lemon Logo"/>
+            <div className={"burger"} onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={`nav_links ${menuOpen ? "open" : ""}`}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/menu">Menu</Link></li>
@@ -14,7 +25,6 @@ function Nav() {
                 <li><Link to="/order">Order Online</Link></li>
                 <li><Link to="/login">Login</Link></li>
             </ul>
-        </div>
     </nav>;
 }
 
