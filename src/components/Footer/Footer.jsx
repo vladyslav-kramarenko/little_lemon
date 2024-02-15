51d14146c27169b779b2e7f955de9c04533ef1ba
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-15 14:22:52 -0800

    restyled footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,22 +4,19 @@ import './Footer.css';
 
 function Footer() {
     return <footer>
-        <div className={"container"}>
-        <div className="doormat">
-            <img className={"logo"} src={logo} alt="Logo"/>
-        </div>
-
-        <div className="navigation">
-            <h3>Doormat Navigation</h3>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/menu">Menu</Link></li>
-                <li><Link to="/booking">Reservations</Link></li>
-                <li><Link to="/order">Order Online</Link></li>
-                <li><Link to="/login">Login</Link></li>
-            </ul>
-        </div>
+        <img className={"logo"} src={logo} alt="Logo"/>
+        <div className={"doormat"}>
+            <div className="navigation">
+                <h3>Doormat Navigation</h3>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/menu">Menu</Link></li>
+                    <li><Link to="/booking">Reservations</Link></li>
+                    <li><Link to="/order">Order Online</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+            </div>
 
             <div className="contact">
                 <h3>Contact</h3>
