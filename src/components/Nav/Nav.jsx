1476f119f70ccf44c292bfc120cd4be1f332e696
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-02-14 19:57:27 -0800

    moved nav to separate folder, added logo

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+import logo from './Logo.png';
+
+function Nav() {
+    return <nav>
+        <div className={"container"}>
+            <ul className={"nav_links"}>
+                <li><img className={"logo"} src={logo} alt="Logo"/></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/menu">Menu</Link></li>
+                <li><Link to="/booking">Reservations</Link></li>
+                <li><Link to="/order">Order Online</Link></li>
+                <li><Link to="/login">Login</Link></li>
+            </ul>
+        </div>
+    </nav>;
+}
+
+export default Nav;
