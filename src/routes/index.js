e6b85ee024d43933021e287ec330ad7bdcd2a06e
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2024-02-17 11:53:16 -0800

    commented out about path for now

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,14 @@ import Menu from '../components/Menu';
 import Contacts from '../components/Contacts';
 import About from '../components/About';
 import Main from "../components/Main";
-import Booking from "../components/Booking";
-import ConfirmedBooking from "../components/ConfirmedBooking";
+import Booking from "../components/Booking/Booking";
+import ConfirmedBooking from "../components/Booking/ConfirmedBooking";
 
 const Routes = () => (
     <RouterRoutes>
         <Route path="/menu" element={<Menu/>}/>
         <Route path="/contacts" element={<Contacts/>}/>
-        <Route path="/about" element={<About/>}/>
+        {/*<Route path="/about" element={<About/>}/>*/}
         <Route path="/booking" element={<Booking/>}/>
         <Route path="/thank-you" element={<ConfirmedBooking />} />
         <Route path="*" element={<Main/>}/>
