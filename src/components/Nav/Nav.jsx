f08d8eb2184e73dfb18d6ee87a35525a55abce4c
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2024-02-17 11:56:42 -0800

    added hom link for logo and some animations to menu links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -11,20 +11,23 @@ function Nav() {
     };
 
     return <nav>
+        <Link to="/">
             <img className={"nav_logo"} src={logo} alt="Little Lemon Logo"/>
-            <div className={"burger"} onClick={toggleMenu}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <ul className={`nav_links ${menuOpen ? "open" : ""}`}>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/menu">Menu</Link></li>
-                <li><Link to="/booking">Reservations</Link></li>
-                <li><Link to="/order">Order Online</Link></li>
-                <li><Link to="/login">Login</Link></li>
-            </ul>
+        </Link>
+
+        <div className={"burger"} onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <ul className={`nav_links ${menuOpen ? "open" : ""}`}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/booking">Reservations</Link></li>
+            <li><Link to="/order">Order Online</Link></li>
+            <li><Link to="/login">Login</Link></li>
+        </ul>
     </nav>;
 }
 
